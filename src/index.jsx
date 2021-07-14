2f1f85245d4494a0d4c51f9ccd854e347ee0f397
professionalsixoneg <76429182+professionalsixoneg@users.noreply.github.com>
2021-07-14 21:24:05 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,10 @@ import _ from 'lodash';
 import { react } from 'react';
 import { render } from 'react-dom';
 
-document.body.appendChild('<div id="root"></div>');
+// Setup root node where our React app will be attached to
+const root = document.createElement("div");
+
+root.id = "root";
+document.body.appendChild(root);
 
 render(<h1>Hello World!</h1>, document.getElementById('root'));
