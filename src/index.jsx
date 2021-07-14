8e7ff7c6b3603af6997982bea9b29c5dbfceca4a
professionalsixoneg <professional.six.one@gmail.com>
2021-07-14 21:47:00 +0530

    1 file modified

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import _ from 'lodash';
-import { react } from 'react';
+import { React } from 'react';
 import { render } from 'react-dom';
 
 // Setup root node where our React app will be attached to
-const root = document.createElement("div");
+const root = document.createElement('div');
 
-root.id = "root";
+root.id = 'root';
 document.body.appendChild(root);
 
 render(<h1>Hello World!</h1>, document.getElementById('root'));
